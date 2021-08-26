baf93d23bf4722f76d35dec6015011e70b63f82a
talk4info <81502381+talk4info@users.noreply.github.com>
2021-08-26 17:25:37 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,60 +20,17 @@ resource "aws_subnet" "main-public-1" {
   tags = {
     Name = "main-public-1"
   }
-}
 
-resource "aws_subnet" "main-public-2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.31.32.0/20"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-2b"
-
-  tags = {
-    Name = "main-public-2"
-  }
-}
-
-resource "aws_subnet" "main-public-3" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.31.16.0/20"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "eu-west-2a"
-
-  tags = {
-    Name = "main-public-3"
-  }
 }
 
 resource "aws_subnet" "main-private-1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "172.31.64.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-2a"
-
-  tags = {
-    Name = "main-private-1"
-  }
-}
-
-resource "aws_subnet" "main-private-2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.31.65.0/24"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "eu-west-2b"
-
-  tags = {
-    Name = "main-private-2"
-  }
-}
-
-resource "aws_subnet" "main-private-3" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.31.66.0/24"
-  map_public_ip_on_launch = "false"
   availability_zone       = "eu-west-2c"
 
   tags = {
-    Name = "main-private-3"
+    Name = "main-private-1"
   }
 }
 
@@ -104,14 +61,3 @@ resource "aws_route_table_association" "main-public-1-a" {
   subnet_id      = aws_subnet.main-public-1.id
   route_table_id = aws_route_table.main-public.id
 }
-
-resource "aws_route_table_association" "main-public-2-a" {
-  subnet_id      = aws_subnet.main-public-2.id
-  route_table_id = aws_route_table.main-public.id
-}
-
-resource "aws_route_table_association" "main-public-3-a" {
-  subnet_id      = aws_subnet.main-public-3.id
-  route_table_id = aws_route_table.main-public.id
-}
-
